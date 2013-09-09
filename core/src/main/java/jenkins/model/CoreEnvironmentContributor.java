32680e1f1f02d94dcf0f08c427a7be1aa775e654
Vincent Latombe <vincent@latombe.net>
2013-09-09 08:52:02 +0200

    Inject computer environment for a run

--- core/src/main/java/jenkins/model/CoreEnvironmentContributor.java
+++ core/src/main/java/jenkins/model/CoreEnvironmentContributor.java
@@ -24,6 +24,11 @@ import java.io.IOException;
 public class CoreEnvironmentContributor extends EnvironmentContributor {
     @Override
     public void buildEnvironmentFor(Run r, EnvVars env, TaskListener listener) throws IOException, InterruptedException {
+        Computer c = Computer.currentComputer();
+        if (c!=null){
+            EnvVars compEnv = c.getEnvironment().overrideAll(env);
+            env.putAll(compEnv);
+        }
         env.put("BUILD_DISPLAY_NAME",r.getDisplayName());
 
         Jenkins j = Jenkins.getInstance();
